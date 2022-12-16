7f07f4acd6719bd4b8d8b1836c56ae286a9433bb
Bakaoh <batatsar@gmail.com>
2022-12-16 09:17:14 +0700

    Update status

--- src/index.js
+++ src/index.js
@@ -4,19 +4,31 @@ require('dotenv').config({ path: path.join(__dirname, '../.env') });
 const fs = require('fs');
 const express = require('express');
 const bodyParser = require('body-parser');
+let startMs = 0;
 
 const app = express();
 app.use(bodyParser.raw());
 
-app.post('/backup', async (req, res) => {
-    const filename = req.query.filename;
-    console.log(filename)
-    fs.writeFileSync(filename, req.body);
+app.get('/status', async (req, res) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
     return res.json({ status: 'ok' });
 });
 
+app.post('/backup', async (req, res) => {
+    const filename = req.query.filename;
+    console.log(filename)
+    fs.writeFileSync(`backup/${filename}`, req.body);
+    return res.json({
+        "initialized": true,
+        "testnet": false,
+        "start_ts": startMs,
+        "service": "KeyBackup",
+        "version": "0.0.1"
+    });
+});
+
 async function start(port) {
-    const startMs = Date.now();
+    startMs = Date.now();
 
     app.listen(port);
 
